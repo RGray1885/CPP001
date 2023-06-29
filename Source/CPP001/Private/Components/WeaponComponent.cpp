539a3f377fc6ada5129b5e292574efab735197ce
RGray1885 <118619520+RGray1885@users.noreply.github.com>
2023-06-30 00:37:47 +0300

    Added weapon component

---
+++ Source/CPP001/Private/Components/WeaponComponent.cpp
@@ -0,0 +1,34 @@
+// Trying to make a simple game using c++ and a lot of help
+
+
+#include "Components/WeaponComponent.h"
+
+// Sets default values for this component's properties
+UWeaponComponent::UWeaponComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UWeaponComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UWeaponComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/CPP001/Public/Components/WeaponComponent.h
@@ -0,0 +1,28 @@
+// Trying to make a simple game using c++ and a lot of help
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "WeaponComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CPP001_API UWeaponComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UWeaponComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
